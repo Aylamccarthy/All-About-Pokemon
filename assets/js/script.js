ec5c6f20ca7312d5f674342d72b00623311c6349
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-02-16 20:43:45 +0000

    Add modal for total score content

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,8 +112,9 @@ function checkAnswer() {
 
 //Gets the current score from the DOM and increments it by 1
 function incrementScore() {
+  let correctScore =  document.getElementById('correct-answer').innerText;
   correctScore = correctScore + 1;
-  document.getElementById('correct-answer').innerText = correctScore;
+
 }
 // Gets the current tally of incorrect answers from the DOM and increase it by 1
 function incrementWrongAswer() {
@@ -122,6 +123,32 @@ function incrementWrongAswer() {
 
 }
 
+/* Modal */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
