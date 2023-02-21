86e9e6bee49b1750490a9cb20a0b5f3880750245
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-02-21 21:20:00 +0000

    Add modal to display game instruction

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,6 +165,21 @@ function disableAnswerButtons() {
           }
         }
 }
+/* Modal */
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', () => {
+  modal.showModal();
+})
+
+closeModal.addEventListener('click', () => {
+  modal.close();
+})
+
+
+
 
 
 
